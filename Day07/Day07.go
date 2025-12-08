02c795d0ae9c741b9e48c97b869bd0c2798cc67a
Arranun <karliosos65@googlemail.com>
2025-12-08 04:02:58 +0100

    Day07 Part2

--- Day07/Day07.go
+++ Day07/Day07.go
@@ -4,14 +4,14 @@ import (
 	"AOC2025/helper"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type tachyonGrid struct {
-	currentPos map[[2]int]bool
-	visited    map[[2]int]bool
-	splitters  map[[2]int]bool
-	grid       helper.Grid
+	currentSplitter map[[2]int]bool
+	visitedSplitter map[[2]int][][2]int
+	grid            helper.Grid
 }
 
 func main() {
@@ -23,37 +23,106 @@ func main() {
 	lines := strings.Split(string(file), "\r\n")
 	tachyon := tachyonGrid{}
 	tachyon.grid = helper.GetGrid(lines)
-	tachyon.visited = map[[2]int]bool{}
-	tachyon.currentPos = map[[2]int]bool{}
-	tachyon.splitters = map[[2]int]bool{}
+	tachyon.visitedSplitter = map[[2]int][][2]int{}
+	tachyon.currentSplitter = map[[2]int]bool{}
+	firstSplitter := [2]int{0, 0}
 	for _, point := range tachyon.grid.Points {
 		if point.Symbol == 'S' {
-			tachyon.currentPos[[2]int{point.X, point.Y}] = true
+			for i := point.Y; i < tachyon.grid.Borders[1]; i++ {
+				if tachyon.grid.Points[[2]int{point.X, i}].Symbol == '^' {
+					tachyon.currentSplitter[[2]int{point.X, i}] = true
+					firstSplitter = [2]int{point.X, i}
+					break
+				}
+			}
 		}
 	}
-	for len(tachyon.currentPos) > 0 {
+	for len(tachyon.currentSplitter) > 0 {
 		tachyon.step()
 	}
-	fmt.Println(len(tachyon.splitters))
+	fmt.Println(len(tachyon.visitedSplitter))
+	for s, e := range tachyon.visitedSplitter {
+		newE := [][2]int{}
+		for _, v := range e {
+			if v != [2]int{-1, -1} {
+				newE = append(newE, v)
+			}
+		}
+		tachyon.visitedSplitter[s] = newE
+	}
+	amount := map[[2]int]int{}
+	finished := map[[2]int]bool{}
+	for s, v := range tachyon.visitedSplitter {
+		amount[s] = 1
+		if len(v) == 0 {
+			finished[s] = true
+		}
+	}
+	for len(finished) > 0 {
+		fin := [2]int{0, 0}
+		for f, _ := range finished {
+			fin = f
+			break
+		}
+		delete(finished, fin)
+		for s, v := range tachyon.visitedSplitter {
+			if len(tachyon.visitedSplitter[s]) == 0 {
+				continue
+			}
+			newV := [][2]int{}
+			for _, v1 := range v {
+				if v1 == fin {
+					amount[s] += amount[fin]
+
+				} else {
+					newV = append(newV, v1)
+				}
+			}
+			tachyon.visitedSplitter[s] = newV
+			if len(tachyon.visitedSplitter[s]) == 0 {
+				finished[s] = true
+			}
+		}
+	}
+	fmt.Println(amount[firstSplitter] + 1)
 }
 
-func (t *tachyonGrid) step() bool {
+func printAmount(tachyon tachyonGrid, amount map[[2]int]int) {
+	for i := 0; i < tachyon.grid.Borders[1]; i++ {
+		str := ""
+		for j := 0; j < tachyon.grid.Borders[0]; j++ {
+			if amount[[2]int{j, i}] > 0 {
+				str += strconv.Itoa(amount[[2]int{j, i}])
+			} else {
+				str += "."
+			}
+		}
+		fmt.Println(str)
+	}
+}
+
+func (t *tachyonGrid) step() {
 	curr := [2]int{0, 0}
-	for p, _ := range t.currentPos {
-		delete(t.currentPos, p)
-		if !t.visited[p] {
-			t.visited[p] = true
+	for p, _ := range t.currentSplitter {
+		delete(t.currentSplitter, p)
+		if t.visitedSplitter[p] == nil {
 			curr = p
 			break
 		}
 	}
-	for i := curr[1]; i < t.grid.Borders[1]; i++ {
-		if t.grid.Points[[2]int{curr[0], i}].Symbol == '^' {
-			t.splitters[[2]int{curr[0], i}] = true
-			t.currentPos[[2]int{curr[0] + 1, i}] = true
-			t.currentPos[[2]int{curr[0] + -1, i}] = true
-			return true
+	currPos := [][2]int{{curr[0] + 1, curr[1]}, {curr[0] - 1, curr[1]}}
+	for _, p := range currPos {
+		i := p[1]
+		for i < t.grid.Borders[1] {
+			if t.grid.Points[[2]int{p[0], i}].Symbol == '^' {
+				t.visitedSplitter[curr] = append(t.visitedSplitter[curr], [2]int{p[0], i})
+				t.currentSplitter[[2]int{p[0], i}] = true
+				break
+			}
+			i++
+		}
+		if i == t.grid.Borders[1] {
+			t.visitedSplitter[curr] = append(t.visitedSplitter[curr], [2]int{-1, -1})
 		}
 	}
-	return false
 }
